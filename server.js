a66948d14db8d4c4491f96c634b972d5c008567a
Matt Williamson <mathewbwilliamson@gmail.com>
2018-09-04 14:53:17 -0400

    first get done

--- server.js
+++ server.js
@@ -1,8 +1,20 @@
 'use strict';
 
+const express = require('express');
+
 // Load array of notes
 const data = require('./db/notes');
 
-console.log('Hello Noteful!');
+const app = express(); 
 
-// INSERT EXPRESS APP CODE HERE...
+//Get Stuff
+app.get('/api/notes', (req, res) => {
+  res.json(data);
+})
+
+
+app.listen(8080, function() {
+  console.info(`Server listening on ${this.address().port}`);
+}).on('error', err => {
+  console.error(err);
+})
