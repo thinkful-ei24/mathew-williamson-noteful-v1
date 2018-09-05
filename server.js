a04c5d171ecdd9722234fbcbc54cc714f6a1ad58
Matt Williamson <mathewbwilliamson@gmail.com>
2018-09-05 14:21:26 -0400

    Added config.js

--- server.js
+++ server.js
@@ -1,33 +1,48 @@
 'use strict';
 
 const express = require('express');
+const config = require('./config');
 
 // Load array of notes
 const data = require('./db/notes');
 
 const app = express(); 
 
+console.log(config.PORT)
+
+const getItemID = function(req) {
+  return Number(req.params.id);
+};
+
+const generateResponse = function(res, jsonObj) {
+  res.json(jsonObj);
+};
+
+const filterBySearchTerm = function(searchQuery) {
+  return data.filter(item => item.title.includes(searchQuery.searchTerm) || item.content.includes(searchQuery.searchTerm));
+}
+
 //Get Stuff
 app.get('/api/notes', (req, res) => {
   const searchQuery = req.query;
 
   if (searchQuery.searchTerm) {
-    let searchedItems = data.filter(item => item.title.includes(searchQuery.searchTerm) || item.content.includes(searchQuery.searchTerm));
-    res.json(searchedItems);
+    let searchedItems = filterBySearchTerm(searchQuery);
+    generateResponse(res, searchedItems);
 
   }
-  res.json(data);
+  generateResponse(res, data);
   return;
-})
+});
 
 app.get('/api/notes/:id', (req, res) => {
-  res.json(data.find(item => item.id === Number(req.params.id)));
+  generateResponse(res, data.find(item => item.id === getItemID(req)));
   return;
-})
+});
 
 
-app.listen(8080, function() {
+app.listen(config.PORT, function() {
   console.info(`Server listening on ${this.address().port}`);
 }).on('error', err => {
   console.error(err);
-})
+});
