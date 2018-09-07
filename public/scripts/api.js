fd5d844aa3b0bb07f833b6c1a2f348da1c2c1059
Matt Williamson <mathewbwilliamson@gmail.com>
2018-09-07 15:03:50 -0400

    Finished api functions to return Promises

--- public/scripts/api.js
+++ public/scripts/api.js
@@ -3,15 +3,7 @@
 
 const api = {
 
-  // search: function (query, callback) {
-  //   $.ajax({
-  //     type: 'GET',
-  //     url: '/api/notes/',
-  //     dataType: 'json',
-  //     data: query,
-  //     success: callback
-  //   });
-  // },
+  /* Promises Done */
   search: function(query) {
     return $.ajax({
       type: 'GET',
@@ -21,45 +13,46 @@ const api = {
     });
   },
 
-  details: function (id, callback) {
-    $.ajax({
+  /* Promises Done */
+  details: function(id) {
+    return $.ajax({
       type: 'GET',
       dataType: 'json',
-      url: `/api/notes/${id}`,
-      success: callback
+      url: `/api/notes/${id}`
     });
   },
-
-  update: function (id, obj, callback) {
-    $.ajax({
+  
+  /* Promises Done */
+  update: function(id, obj) {
+    return $.ajax({
       type: 'PUT',
       url: `/api/notes/${id}`,
       contentType: 'application/json',
       dataType: 'json',
-      data: JSON.stringify(obj),
-      success: callback
+      data: JSON.stringify(obj)
     });
   },
 
-  create: function (obj, callback) {
-    $.ajax({
+  /* Promises Done */
+  create: function( obj ) {
+    return $.ajax({
       type: 'POST',
       url: '/api/notes',
       contentType: 'application/json',
       dataType: 'json',
       processData: false,
-      data: JSON.stringify(obj),
-      success: callback
+      data: JSON.stringify(obj)
     });
   },
 
-  remove: function (id, callback) {
+  /* Promises Done */
+  remove: function( id ) {
     return $.ajax({
       type: 'DELETE',
       url: `/api/notes/${id}`,
-      dataType: 'json',
-      success: callback
+      dataType: 'json'
     });
   }
+
 
 };
